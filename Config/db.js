ab67fe6fef44602bf93b96f033b4697a119c2984
akhtarfaizan20 <akhtarfaizan20@gmail.com>
2023-04-08 16:12:38 +0530

    fixed

--- Config/db.js
+++ Config/db.js
@@ -3,7 +3,9 @@ const { default: mongoose } = require("mongoose");
 require("dotenv").config();
 mongoose.set("strictQuery", false);
 
-const connectDB = mongoose.connect(process.env.mongoURl);
+const connectDB = mongoose.connect(
+  "mongodb+srv://faizan:faizan@cluster0.7vwn8xg.mongodb.net/mock14?retryWrites=true&w=majority"
+);
 module.exports = {
   connectDB,
 };
